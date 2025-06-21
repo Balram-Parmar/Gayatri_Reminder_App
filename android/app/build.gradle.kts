a11943331ea6d802fbe64cf598f2b347d77d0525
HareKrishnaMandir <harekrishnamandir.ahmd@gmail.com>
2025-06-21 13:01:27 +0530

    no message

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,10 +8,9 @@ plugins {
 android {
     namespace = "com.hkm.gayatri"
     compileSdk = 35
-   ndkVersion = "27.0.12077973"    
+    ndkVersion = "27.0.12077973"    
    
-   
-   compileOptions {
+    compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
@@ -19,11 +18,9 @@ android {
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_11.toString()
     }
-      defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+    
+    defaultConfig {
         applicationId = "com.hkm.gayatri"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
         targetSdk = 35
         versionCode = flutter.versionCode
@@ -32,9 +29,9 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            // No signing config - will create unsigned APK
+            isMinifyEnabled = false
+            isShrinkResources = false
         }
     }
 }
